fecedd89bfff32b683aed119d55f75f8ddc12881
NayabAkhtar12 <119120061+NayabAkhtar12@users.noreply.github.com>
2023-10-11 21:55:06 +0500

    Started POM

---
+++ TestProject1/TestScripts/Module1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestProject1;
+
+namespace Webdriveruniversity_AutomationProject.TestScripts
+{
+    [TestFixture]
+    public class Module1 :Baseclass
+    {
+        [Test]
+       public void TestMethod1()
+        {
+
+        }
+    }
+}
